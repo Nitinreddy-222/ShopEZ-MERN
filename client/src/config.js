21069a40c6ab9153f6d3a65f17a0ced6c49e1ee1
Nitin Reddy <vakiti.nitinreddy@gmail.com>
2026-06-18 20:51:01 +0530

    Delete client/src/config.js

--- client/src/config.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = "https://shopez-mern.onrender.com";
-import { API_URL } from '../config';
-
-const { data } = await axios.post(
-  `${API_URL}/api/users/login`,
-  { email, password }
-);
