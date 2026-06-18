ffb9f98d3578683c08e68508a08e3a527853ac18
Nitin Reddy <vakiti.nitinreddy@gmail.com>
2026-06-18 20:10:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+console.log("MONGO_URI FOUND:", process.env.MONGO_URI ? "YES" : "NO");
+console.log("URI VALUE:", process.env.MONGO_URI);
 
 // Load env variables if .env exists
 dotenv.config();
